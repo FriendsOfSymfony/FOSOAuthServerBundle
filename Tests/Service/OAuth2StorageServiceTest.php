5ff308ebc9164cac925c5526c5d333990c48aa6e
Arnaud Le Blanc <arnaud.lb@gmail.com>
2011-11-28 22:07:45 +0100

    more tests

--- Tests/Service/OAuth2StorageServiceTest.php
+++ Tests/Service/OAuth2StorageServiceTest.php
@@ -4,19 +4,24 @@ namespace Alb\OAuth2ServiceBundle\Tests\Service;
 
 use Alb\OAuth2ServerBundle\Model\OAuth2Client;
 use Alb\OAuth2ServerBundle\Service\OAuth2StorageService;
+use Alb\OAuth2ServerBundle\Model\OAuth2AccessToken;
 
 class OAuth2StorageServiceTest extends \PHPUnit_Framework_TestCase
 {
     protected $clientManager;
     protected $accessTokenManager;
+    protected $userProvider;
+    protected $encoderFactory;
     protected $storage;
 
     public function setUp()
     {
         $this->clientManager = $this->getMock('Alb\OAuth2ServerBundle\Model\OAuth2ClientManagerInterface');
         $this->accessTokenManager = $this->getMock('Alb\OAuth2ServerBundle\Model\OAuth2AccessTokenManagerInterface');
+        $this->userProvider = $this->getMock('Symfony\Component\Security\Core\User\UserProviderInterface');
+        $this->encoderFactory = $this->getMock('Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface');
 
-        $this->storage = new OAuth2StorageService($this->clientManager, $this->accessTokenManager);
+        $this->storage = new OAuth2StorageService($this->clientManager, $this->accessTokenManager, $this->userProvider, $this->encoderFactory);
     }
 
     public function testGetClientReturnsClientWithGivenId()
@@ -30,5 +35,197 @@ class OAuth2StorageServiceTest extends \PHPUnit_Framework_TestCase
 
         $this->assertSame($client, $this->storage->getClient('123_abc'));
     }
+
+    public function testGetClientReturnsNullIfNotExists()
+    {
+        $client = new OAuth2Client;
+
+        $this->clientManager->expects($this->once())
+            ->method('findClientByPublicId')
+            ->with('123_abc')
+            ->will($this->returnValue(null));
+
+        $this->assertNull($this->storage->getClient('123_abc'));
+    }
+
+    public function testCheckClientCredentialsThrowsIfInvalidClientClass()
+    {
+        $client = $this->getMock('OAuth2\Model\IOAuth2Client');
+
+        $this->setExpectedException('InvalidArgumentException');
+        $this->storage->checkClientCredentials($client, 'dummy');
+    }
+
+    public function testCheckClientCredentialsReturnsTrueOnValidCredentials()
+    {
+        $client = new OAuth2Client;
+        $client->setSecret('dummy');
+
+        $this->assertTrue($this->storage->checkClientCredentials($client, 'dummy'));
+    }
+
+    public function testCheckClientCredentialsReturnsFalseOnValidCredentials()
+    {
+        $client = new OAuth2Client;
+        $client->setSecret('dummy');
+
+        $this->assertFalse($this->storage->checkClientCredentials($client, 'passe'));
+    }
+
+    public function testGetAccessTokenReturnsAccessTokenWithGivenId()
+    {
+        $token = new OAuth2AccessToken;
+
+        $this->accessTokenManager->expects($this->once())
+            ->method('findTokenByToken')
+            ->with('123_abc')
+            ->will($this->returnValue($token));
+
+        $this->assertSame($token, $this->storage->getAccessToken('123_abc'));
+    }
+
+    public function testGetAccessTokenReturnsNullIfNotExists()
+    {
+        $token = new OAuth2AccessToken;
+
+        $this->accessTokenManager->expects($this->once())
+            ->method('findTokenByToken')
+            ->with('123_abc')
+            ->will($this->returnValue(null));
+
+        $this->assertNull($this->storage->getAccessToken('123_abc'));
+    }
+
+    public function testCreateAccessTokenThrowsOnInvalidClientClass()
+    {
+        $client = $this->getMock('OAuth2\Model\IOAuth2Client');
+
+        $this->setExpectedException('InvalidArgumentException');
+        $this->storage->createAccessToken('foo', $client, 42, 1, 'foo bar');
+    }
+
+    public function testCreateAccessToken()
+    {
+        $savedToken = null;
+
+        $this->accessTokenManager->expects($this->once())
+            ->method('createToken')
+            ->with()
+            ->will($this->returnValue(new OAuth2AccessToken));
+        $this->accessTokenManager->expects($this->once())
+            ->method('updateToken')
+            ->will($this->returnCallback(function($token) use (&$savedToken) {
+                $savedToken = $token;
+            }));
+
+        $client = new OAuth2Client;
+
+        $token = $this->storage->createAccessToken('foo', $client, 42, 1, 'foo bar');
+
+        $this->assertEquals($token, $savedToken);
+
+        $this->assertSame('foo', $token->getToken());
+        $this->assertSame($client, $token->getClient());
+        $this->assertSame(42, $token->getData());
+        $this->assertSame(1, $token->getExpiresAt());
+        $this->assertSame('foo bar', $token->getScope());
+    }
+
+    public function testCheckRestrictedGrantTypeThrowsOnInvalidClientClass()
+    {
+        $client = $this->getMock('OAuth2\Model\IOAuth2Client');
+
+        $this->setExpectedException('InvalidArgumentException');
+
+        $this->storage->checkRestrictedGrantType($client, 'foo');
+    }
+
+    public function testCheckRestrictedGrantType()
+    {
+        $client = new OAuth2Client;
+        $client->setAllowedGrantTypes(array('foo', 'bar'));
+
+        $this->assertTrue($this->storage->checkRestrictedGrantType($client, 'foo'));
+        $this->assertTrue($this->storage->checkRestrictedGrantType($client, 'bar'));
+        $this->assertFalse($this->storage->checkRestrictedGrantType($client, 'baz'));
+    }
+
+    public function testCheckUserCredentialsThrowsOnInvalidClientClass()
+    {
+        $client = $this->getMock('OAuth2\Model\IOAuth2Client');
+
+        $this->setExpectedException('InvalidArgumentException');
+
+        $this->storage->checkUserCredentials($client, 'Joe', 'baz');
+    }
+
+    public function testCheckUserCredentialsReturnsTrueOnValidCredentials()
+    {
+        $client = new OAuth2Client;
+        $user = $this->getMock('Symfony\Component\Security\Core\User\UserInterface');
+        $user->expects($this->once())
+            ->method('getPassword')->with()->will($this->returnValue('foo'));
+        $user->expects($this->once())
+            ->method('getSalt')->with()->will($this->returnValue('bar'));
+
+        $encoder = $this->getMock('Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface');
+        $encoder->expects($this->once())
+            ->method('isPasswordValid')
+            ->with('foo', 'baz', 'bar')
+            ->will($this->returnValue(true));
+
+        $this->userProvider->expects($this->once())
+            ->method('loadUserByUsername')
+            ->with('Joe')
+            ->will($this->returnValue($user));
+
+        $this->encoderFactory->expects($this->once())
+            ->method('getEncoder')
+            ->with($user)
+            ->will($this->returnValue($encoder));
+
+        $this->assertTrue($this->storage->checkUserCredentials($client, 'Joe', 'baz'));
+    }
+
+    public function testCheckUserCredentialsReturnsFalseOnInvalidCredentials()
+    {
+        $client = new OAuth2Client;
+        $user = $this->getMock('Symfony\Component\Security\Core\User\UserInterface');
+        $user->expects($this->once())
+            ->method('getPassword')->with()->will($this->returnValue('foo'));
+        $user->expects($this->once())
+            ->method('getSalt')->with()->will($this->returnValue('bar'));
+
+        $encoder = $this->getMock('Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface');
+        $encoder->expects($this->once())
+            ->method('isPasswordValid')
+            ->with('foo', 'baz', 'bar')
+            ->will($this->returnValue(false));
+
+        $this->userProvider->expects($this->once())
+            ->method('loadUserByUsername')
+            ->with('Joe')
+            ->will($this->returnValue($user));
+
+        $this->encoderFactory->expects($this->once())
+            ->method('getEncoder')
+            ->with($user)
+            ->will($this->returnValue($encoder));
+
+        $this->assertFalse($this->storage->checkUserCredentials($client, 'Joe', 'baz'));
+    }
+
+    public function testCheckUserCredentialsReturnsFalseIfUserNotExist()
+    {
+        $client = new OAuth2Client;
+
+        $this->userProvider->expects($this->once())
+            ->method('loadUserByUsername')
+            ->with('Joe')
+            ->will($this->returnValue(null));
+
+        $this->assertFalse($this->storage->checkUserCredentials($client, 'Joe', 'baz'));
+    }
+
 }
 
