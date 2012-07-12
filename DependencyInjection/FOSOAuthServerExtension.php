1562c278536f319b334d35ab29d210e7eaa08ba7
Joshua Thijssen <jthijssen@noxlogic.nl>
2012-07-12 12:06:50 +0200

    added user_provider argument to storage service

--- DependencyInjection/FOSOAuthServerExtension.php
+++ DependencyInjection/FOSOAuthServerExtension.php
@@ -43,6 +43,7 @@ class FOSOAuthServerExtension extends Extension
         $container->setAlias('fos_oauth_server.access_token_manager', $config['service']['access_token_manager']);
         $container->setAlias('fos_oauth_server.refresh_token_manager', $config['service']['refresh_token_manager']);
         $container->setAlias('fos_oauth_server.auth_code_manager', $config['service']['auth_code_manager']);
+        $container->setAlias('fos_oauth_server.user_provider', $config['service']['user_provider']);
 
         $container->setParameter('fos_oauth_server.server.options', $config['service']['options']);
 
