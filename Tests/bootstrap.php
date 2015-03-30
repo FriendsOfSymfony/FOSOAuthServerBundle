9a38a63cbc7a7cedcf0063e0bb89b50187f1fca9
Geoffrey Vincent <g.vincent@soqrate.com>
2015-03-30 14:53:35 +0200

    Update generating propel base classes required for testing

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -12,19 +12,15 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // require Propel
-if (file_exists($file = __DIR__ . '/../vendor/propel/propel1/generator/lib/util/PropelQuickBuilder.php')) {
-    set_include_path(__DIR__ . '/../vendor/phing/phing/classes' . PATH_SEPARATOR . get_include_path());
+if (file_exists($file = __DIR__ . '/../vendor/propel/propel/src/Propel/Generator/Util/QuickBuilder.php')) {
     require_once $file;
 }
 
 \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader('class_exists');
 
 // Generate Propel base classes on the fly
-if (class_exists('TypehintableBehavior')) {
-    $class   = new \ReflectionClass('TypehintableBehavior');
-    $builder = new \PropelQuickBuilder();
-    $builder->getConfig()->setBuildProperty('behavior.typehintable.class', $class->getFileName());
-    $builder->setSchema(file_get_contents(__DIR__ . '/../Resources/config/propel/schema.xml'));
-    $builder->setClassTargets(array('tablemap', 'peer', 'object', 'query', 'peerstub'));
-    $builder->build();
-}
+$builder = new \QuickBuilder();
+$builder->setSchema(file_get_contents(__DIR__ . '/../Resources/config/propel/schema.xml'));
+$builder->setClassTargets(array('tablemap', 'object', 'query'));
+$builder->build();
+
