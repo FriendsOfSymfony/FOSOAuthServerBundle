54f8f2227552f86ccf427bcf43bb0dfff9a3ccfd
Konrad Rozner <konrad.rozner@gmail.com>
2016-02-09 22:07:40 +0100

    feedback fixes

--- Controller/TokenController.php
+++ Controller/TokenController.php
@@ -12,29 +12,18 @@
 namespace FOS\OAuthServerBundle\Controller;
 
 use FOS\OAuthServerBundle\Event\OAuthTokenEvent;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use OAuth2\OAuth2;
 use OAuth2\OAuth2ServerException;
 use Symfony\Component\HttpFoundation\Response;
 
-class TokenController implements ContainerAwareInterface
+class TokenController
 {
     /**
      * @var ContainerInterface
      */
     protected $container;
-
-    /**
-     * Sets the container.
-     *
-     * @param ContainerInterface|null $container A ContainerInterface instance or null
-     */
-    public function setContainer(ContainerInterface $container = null)
-    {
-        $this->container = $container;
-    }
 
     /**
      * @var OAuth2
@@ -43,10 +32,12 @@ class TokenController implements ContainerAwareInterface
 
     /**
      * @param OAuth2 $server
+     * @param ContainerInterface $container
      */
-    public function __construct(OAuth2 $server)
+    public function __construct(OAuth2 $server, ContainerInterface $container)
     {
         $this->server = $server;
+        $this->container = $container;
     }
 
     /**
@@ -58,23 +49,26 @@ class TokenController implements ContainerAwareInterface
         try
         {
             $response = $this->server->grantAccessToken($request);
-
-            $data = json_decode($response->getContent(), true);
-
-            $storage = $this->container->get('fos_oauth_server.storage');
-            /* @var $storage \FOS\OAuthServerBundle\Storage\OAuthStorage */
-            $accessToken = $storage->getAccessToken($data[OAuth2::TOKEN_PARAM_NAME]);
-            /* @var $accessToken \FOS\OAuthServerBundle\Entity\AccessToken */
-
-            $this->container->get('event_dispatcher')->dispatch(
-                OAuthTokenEvent::POST_ACCESS_TOKEN_GRANT,
-                new OAuthTokenEvent($accessToken)
-            );
-
-            return $response;
         }
         catch (OAuth2ServerException $e) {
             return $e->getHttpResponse();
         }
+
+        $data = json_decode($response->getContent(), true);
+
+        $storage = $this->container->get('fos_oauth_server.storage');
+        /* @var $storage \FOS\OAuthServerBundle\Storage\OAuthStorage */
+        $accessToken = $storage->getAccessToken($data[OAuth2::TOKEN_PARAM_NAME]);
+        /* @var $accessToken \FOS\OAuthServerBundle\Entity\AccessToken */
+
+        if($this->container->has('event_dispatcher'))
+        {
+            $this->container->get('event_dispatcher')->dispatch(
+                OAuthTokenEvent::POST_ACCESS_TOKEN_GRANT,
+                new OAuthTokenEvent($accessToken)
+            );
+        }
+
+        return $response;
     }
 }
