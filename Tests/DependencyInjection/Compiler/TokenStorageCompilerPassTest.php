067401e8cbf7f6948bbcf27fb7fffd1e93890f9f
Nikola Petkanski <nikola@petkanski.com>
2018-01-11 16:03:50 +0200

    + added missing unit tests

---
+++ Tests/DependencyInjection/Compiler/TokenStorageCompilerPassTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace FOS\OAuthServerBundle\Tests\DependencyInjection\Compiler;
+
+use FOS\OAuthServerBundle\DependencyInjection\Compiler\TokenStorageCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Class TokenStorageCompilerPassTest
+ * @package FOS\OAuthServerBundle\Tests\DependencyInjection\Compiler
+ * @author Nikola Petkanski <nikola@petkanski.com>
+ */
+class TokenStorageCompilerPassTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var TokenStorageCompilerPass
+     */
+    protected $instance;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|ContainerBuilder
+     */
+    protected $container;
+
+    public function setUp()
+    {
+        $this->container = $this->createMock(ContainerBuilder::class);
+        $this->instance = new TokenStorageCompilerPass();
+
+        parent::setUp();
+    }
+
+    public function testProcessWithExistingTokenStorage()
+    {
+        $authenticationListenerDefinition = $this->createMock(Definition::class);
+
+        $this->container
+            ->expects($this->once())
+            ->method('getDefinition')
+            ->with('fos_oauth_server.security.authentication.listener')
+            ->willReturn($authenticationListenerDefinition)
+        ;
+
+        $this->container
+            ->expects($this->once())
+            ->method('hasDefinition')
+            ->with('security.token_storage')
+            ->willReturn(true)
+        ;
+
+        $this->assertNull($this->instance->process($this->container));
+    }
+
+    public function testProcessWithoutExistingTokenStorage()
+    {
+        $authenticationListenerDefinition = $this->createMock(Definition::class);
+
+        $this->container
+            ->expects($this->once())
+            ->method('getDefinition')
+            ->with('fos_oauth_server.security.authentication.listener')
+            ->willReturn($authenticationListenerDefinition)
+        ;
+
+        $this->container
+            ->expects($this->once())
+            ->method('hasDefinition')
+            ->with('security.token_storage')
+            ->willReturn(false)
+        ;
+
+        $authenticationListenerDefinition
+            ->expects($this->once())
+            ->method('replaceArgument')
+            ->with(
+                0,
+                new Reference('security.context')
+            )
+            ->willReturn(null)
+        ;
+
+        $this->assertNull($this->instance->process($this->container));
+    }
+}
