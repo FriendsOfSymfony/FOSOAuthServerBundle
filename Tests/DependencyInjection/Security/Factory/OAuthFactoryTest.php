4c390ad7af8db1d169ba5d1d8a4f534f7d38413a
Nikola Petkanski <nikola@petkanski.com>
2018-01-12 19:52:34 +0200

    ~ fixed unit test for OAuthFactory to test with ChildDefinition or DefinitionDecorator, depending on what's available

--- Tests/DependencyInjection/Security/Factory/OAuthFactoryTest.php
+++ Tests/DependencyInjection/Security/Factory/OAuthFactoryTest.php
@@ -4,6 +4,7 @@ namespace FOS\OAuthServerBundle\Tests\DependencyInjection\Security\Factory;
 
 use FOS\OAuthServerBundle\DependencyInjection\Security\Factory\OAuthFactory;
 use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\DefinitionDecorator;
@@ -40,6 +41,17 @@ class OAuthFactoryTest extends \PHPUnit_Framework_TestCase
 
     public function testCreate()
     {
+        if (class_exists('Symfony\Component\DependencyInjection\DefinitionDecorator')) {
+            return $this->useDefinitionDecorator();
+        } elseif (class_exists('Symfony\Component\DependencyInjection\ChildDefinition')) {
+            return $this->useChildDefinition();
+        }
+
+        throw new \Exception('Neither DefinitionDecorator nor ChildDefinition exist');
+    }
+
+    protected function useDefinitionDecorator()
+    {
         $container = $this->getMockBuilder(ContainerBuilder::class)
             ->disableOriginalConstructor()
             ->setMethods([
@@ -69,6 +81,59 @@ class OAuthFactoryTest extends \PHPUnit_Framework_TestCase
                 [
                     'security.authentication.listener.fos_oauth_server.'. $id,
                     new DefinitionDecorator('fos_oauth_server.security.authentication.listener')
+                ]
+            )
+            ->willReturnOnConsecutiveCalls(
+                $definition,
+                null
+            )
+        ;
+
+        $definition
+            ->expects($this->once())
+            ->method('replaceArgument')
+            ->with(0, new Reference($userProvider))
+            ->willReturn(null)
+        ;
+
+        $this->assertSame([
+            'security.authentication.provider.fos_oauth_server.'. $id,
+            'security.authentication.listener.fos_oauth_server.'. $id,
+            'fos_oauth_server.security.entry_point'
+        ], $this->instance->create($container, $id, $config, $userProvider, $defaultEntryPoint));
+    }
+
+    protected function useChildDefinition()
+    {
+        $container = $this->getMockBuilder(ContainerBuilder::class)
+            ->disableOriginalConstructor()
+            ->setMethods([
+                'setDefinition',
+
+            ])
+            ->getMock()
+        ;
+        $id = '12';
+        $config = [];
+        $userProvider = 'mock.user.provider.service';
+        $defaultEntryPoint = '';
+
+        $definition = $this->getMockBuilder(Definition::class)
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $container
+            ->expects($this->exactly(2))
+            ->method('setDefinition')
+            ->withConsecutive(
+                [
+                    'security.authentication.provider.fos_oauth_server.'. $id,
+                    new ChildDefinition('fos_oauth_server.security.authentication.provider')
+                ],
+                [
+                    'security.authentication.listener.fos_oauth_server.'. $id,
+                    new ChildDefinition('fos_oauth_server.security.authentication.listener')
                 ]
             )
             ->willReturnOnConsecutiveCalls(
