2b6c04ea514bb17b2dbdfbcb88ce3ecdbac1c504
Nikola Petkanski <dinamic@gmail.com>
2018-01-15 14:12:08 +0200

    Update FOSOAuthServerBundle.php

--- FOSOAuthServerBundle.php
+++ FOSOAuthServerBundle.php
@@ -1,129 +1,55 @@
 <?php
 
-namespace FOS\OAuthServerBundle\Tests;
+declare(strict_types=1);
 
-use FOS\OAuthServerBundle\DependencyInjection\Compiler;
+/*
+ * This file is part of the FOSOAuthServerBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\OAuthServerBundle;
+
+use FOS\OAuthServerBundle\DependencyInjection\Compiler\GrantExtensionsCompilerPass;
+use FOS\OAuthServerBundle\DependencyInjection\Compiler\RequestStackCompilerPass;
+use FOS\OAuthServerBundle\DependencyInjection\Compiler\TokenStorageCompilerPass;
 use FOS\OAuthServerBundle\DependencyInjection\FOSOAuthServerExtension;
 use FOS\OAuthServerBundle\DependencyInjection\Security\Factory\OAuthFactory;
-use FOS\OAuthServerBundle\FOSOAuthServerBundle;
 use Symfony\Bundle\SecurityBundle\DependencyInjection\SecurityExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\HttpKernel\Kernel;
 
-class FOSOAuthServerBundleTest extends \PHPUnit_Framework_TestCase
+class FOSOAuthServerBundle extends Bundle
 {
-    protected function setUp()
+    /**
+     * @example '2.1.0'
+     *
+     * @var string
+     */
+    private $kernelVersion;
+
+    public function __construct()
     {
-        parent::setUp();
+        $this->kernelVersion = Kernel::VERSION;
+        $this->extension = new FOSOAuthServerExtension();
     }
 
-    public function testConstruction()
+    public function build(ContainerBuilder $container)
     {
-        $bundle = new FOSOAuthServerBundle();
+        parent::build($container);
 
-        $objectReflection = new \ReflectionObject($bundle);
+        if (version_compare($this->kernelVersion, '2.1', '>=')) {
+            /** @var SecurityExtension $extension */
+            $extension = $container->getExtension('security');
+            $extension->addSecurityListenerFactory(new OAuthFactory());
+        }
 
-        $propertyReflection = $objectReflection->getProperty('extension');
-        $propertyReflection->setAccessible(true);
-
-        $this->assertInstanceOf(FOSOAuthServerExtension::class, $propertyReflection->getValue($bundle));
-
-        $propertyReflection = $objectReflection->getProperty('kernelVersion');
-        $propertyReflection->setAccessible(true);
-
-        $this->assertEquals(Kernel::VERSION, $propertyReflection->getValue($bundle));
-    }
-
-    public function testBuildForSymfonyHigherThan20()
-    {
-        $bundle = new FOSOAuthServerBundle();
-        $objectReflection = new \ReflectionObject($bundle);
-
-        $propertyReflection = $objectReflection->getProperty('kernelVersion');
-        $propertyReflection->setAccessible(true);
-        $propertyReflection->setValue($bundle, '2.1.0');
-
-        /** @var ContainerBuilder|\PHPUnit_Framework_MockObject_MockObject $containerBuilder */
-        $containerBuilder = $this->getMockBuilder(ContainerBuilder::class)
-            ->disableOriginalConstructor()
-            ->setMethods([
-                'getExtension',
-                'addCompilerPass',
-            ])
-            ->getMock()
-        ;
-
-        /** @var SecurityExtension|\PHPUnit_Framework_MockObject_MockObject $securityExtension */
-        $securityExtension = $this->getMockBuilder(SecurityExtension::class)
-            ->disableOriginalConstructor()
-            ->getMock()
-        ;
-
-        $containerBuilder
-            ->expects($this->at(0))
-            ->method('getExtension')
-            ->with('security')
-            ->willReturn($securityExtension)
-        ;
-
-        $securityExtension
-            ->expects($this->at(0))
-            ->method('addSecurityListenerFactory')
-            ->with(new OAuthFactory())
-            ->willReturn(null)
-        ;
-
-        $containerBuilder
-            ->expects($this->at(1))
-            ->method('addCompilerPass')
-            ->withConsecutive(
-                new Compiler\GrantExtensionsCompilerPass(),
-                new Compiler\TokenStorageCompilerPass(),
-                new Compiler\RequestStackCompilerPass()
-            )
-            ->willReturnOnConsecutiveCalls(
-                $containerBuilder,
-                $containerBuilder,
-                $containerBuilder
-            )
-        ;
-
-        $this->assertNull($bundle->build($containerBuilder));
-    }
-
-    public function testBuildForSymfony20()
-    {
-        $bundle = new FOSOAuthServerBundle();
-        $objectReflection = new \ReflectionObject($bundle);
-
-        $propertyReflection = $objectReflection->getProperty('kernelVersion');
-        $propertyReflection->setAccessible(true);
-        $propertyReflection->setValue($bundle, '2.0.0');
-
-        /** @var ContainerBuilder|\PHPUnit_Framework_MockObject_MockObject $containerBuilder */
-        $containerBuilder = $this->getMockBuilder(ContainerBuilder::class)
-            ->disableOriginalConstructor()
-            ->setMethods([
-                'addCompilerPass',
-            ])
-            ->getMock()
-        ;
-
-        $containerBuilder
-            ->expects($this->at(1))
-            ->method('addCompilerPass')
-            ->withConsecutive(
-                new Compiler\GrantExtensionsCompilerPass(),
-                new Compiler\TokenStorageCompilerPass(),
-                new Compiler\RequestStackCompilerPass()
-            )
-            ->willReturnOnConsecutiveCalls(
-                $containerBuilder,
-                $containerBuilder,
-                $containerBuilder
-            )
-        ;
-
-        $this->assertNull($bundle->build($containerBuilder));
+        $container->addCompilerPass(new GrantExtensionsCompilerPass());
+        $container->addCompilerPass(new TokenStorageCompilerPass());
+        $container->addCompilerPass(new RequestStackCompilerPass());
     }
 }
