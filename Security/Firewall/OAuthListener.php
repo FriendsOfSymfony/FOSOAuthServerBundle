61bf418c4ae770fbe46b8779cc26d65d36236df6
Klapaudius <klapaudius@gmail.com>
2020-09-29 23:00:32 +0200

    Symfony 5 compliant

--- Security/Firewall/OAuthListener.php
+++ Security/Firewall/OAuthListener.php
@@ -16,7 +16,7 @@ namespace FOS\OAuthServerBundle\Security\Firewall;
 use FOS\OAuthServerBundle\Security\Authentication\Token\OAuthToken;
 use OAuth2\OAuth2;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
@@ -58,9 +58,9 @@ class OAuthListener
     }
 
     /**
-     * @param GetResponseEvent $event the event
+     * @param RequestEvent $event the event
      */
-    public function __invoke(GetResponseEvent $event)
+    public function __invoke(RequestEvent $event)
     {
         if (null === $oauthToken = $this->serverService->getBearerToken($event->getRequest(), true)) {
             return;
